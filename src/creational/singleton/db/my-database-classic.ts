2ae5883e157a3a393b379a15566bb67b434a840c
Paulo Jose Ribeiro Silva <paulo.jose@mrv.com.br>
2021-03-04 00:52:59 -0300

    Criando singleton

---
+++ src/creational/singleton/db/my-database-classic.ts
@@ -0,0 +1,21 @@
+export class MyDatabaseClassic {
+  private static instance: MyDatabaseClassic | null = null;
+
+  // eslint-disable-next-line prettier/prettier
+    private constructor() {
+        
+    }
+
+  public static getInstance(): MyDatabaseClassic {
+    if (MyDatabaseClassic.instance === null) {
+      MyDatabaseClassic.instance = new MyDatabaseClassic();
+    }
+
+    return MyDatabaseClassic.instance;
+  }
+}
+
+const db1 = MyDatabaseClassic.getInstance();
+const db2 = MyDatabaseClassic.getInstance();
+
+console.log(db1 === db2);
