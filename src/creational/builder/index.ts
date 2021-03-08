96fd5efb3294bb844b274ff8a19ef9078aee958e
Paulo José Ribeiro Silva <paulotec96@gmail.com>
2021-03-07 22:47:31 -0300

    Creating a builder  for vegan meals

--- src/creational/builder/index.ts
+++ src/creational/builder/index.ts
@@ -1,4 +1,5 @@
 import { MainDishBuilder } from './classes/main-dish-builder';
+import { VeganDishBuilder } from './classes/vegan-dish-builder';
 
 const mainDishBuilder = new MainDishBuilder();
 mainDishBuilder.makeMeal();
@@ -7,3 +8,8 @@ console.log(mainDishBuilder.getPrice());
 mainDishBuilder.reset();
 const meal2 = mainDishBuilder.makeBeverage().getMeal();
 console.log(meal2);
+
+const veganDishBuilder = new VeganDishBuilder();
+const veganMeal = veganDishBuilder.makeMeal().getMeal();
+console.log(veganMeal);
+console.log(veganMeal.getPrice());
