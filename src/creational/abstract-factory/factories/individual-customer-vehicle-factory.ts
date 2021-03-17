c08dc77efdcb3b30a93bfd5f34d5aa89fc5f7e68
Paulo José Ribeiro Silva <paulotec96@gmail.com>
2021-03-17 00:55:00 -0300

    Fixing grammatical mistake

--- src/creational/abstract-factory/factories/individual-customer-vehicle-factory.ts
+++ src/creational/abstract-factory/factories/individual-customer-vehicle-factory.ts
@@ -1,15 +1,15 @@
 import { Customer } from '../customer/interfaces/customer';
 import { VehicleProtocol } from '../customer/interfaces/vehicle-protocol';
-import { InvidualCustomer } from '../customer/individual-customers';
-import { InvidualCar } from '../vehicle/invidual-car';
+import { InvidualCustomer as Individual } from '../customer/individual-customers';
+import { Individual as IndividualCar } from '../vehicle/individual-car';
 
 export class InvidualCreateVehicheCustomerFactory {
   createCustomer(customerName: string): Customer {
-    return new InvidualCustomer(customerName);
+    return new Individual(customerName);
   }
 
   createVehicle(vehicleName: string, customerName: string): VehicleProtocol {
     const customer = this.createCustomer(customerName);
-    return new InvidualCar(vehicleName, customer);
+    return new IndividualCar(vehicleName, customer);
   }
 }
--- src/creational/abstract-factory/vehicle/individual-car.ts
+++ src/creational/abstract-factory/vehicle/individual-car.ts
@@ -1,6 +1,6 @@
 import { VehicleProtocol } from '../customer/interfaces/vehicle-protocol';
 import { Customer } from '../customer/interfaces/customer';
-export class InvidualCar implements VehicleProtocol {
+export class Individual implements VehicleProtocol {
   constructor(public name: string, private readonly customer: Customer) {}
   pickUp(): void {
     console.log(`${this.name} está buscando ${this.customer.name}`);
