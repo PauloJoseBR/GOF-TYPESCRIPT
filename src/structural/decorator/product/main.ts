3758691e789df9ccdee52c18336230288870883a
Paulo José Ribeiro Silva <paulotec96@gmail.com>
2021-03-21 20:34:18 -0300

    Finishing decorator

--- src/structural/decorator/product/main.ts
+++ src/structural/decorator/product/main.ts
@@ -1,9 +1,11 @@
+import { ProductCustomizationDecorator } from './product-customization-decorator';
 import { ProductStampDecorator } from './product-stamp-decoration';
 import { TShirt } from './t-shirt';
 
 const tShirt = new TShirt();
 const decoratedTShirt = new ProductStampDecorator(tShirt);
 const tShirtStampFrontAndBack = new ProductStampDecorator(decoratedTShirt);
+const customizedTshirt = new ProductCustomizationDecorator(tShirt);
 
 console.log(tShirt.getPrice(), tShirt.getName());
 console.log(decoratedTShirt.getPrice(), decoratedTShirt.getName());
@@ -11,3 +13,4 @@ console.log(
   tShirtStampFrontAndBack.getPrice(),
   tShirtStampFrontAndBack.getName(),
 );
+console.log(customizedTshirt.getName(), customizedTshirt.getPrice());
