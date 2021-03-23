ae0665a4ec9d1bf771dbc97052aedeb20e634163
Paulo José Ribeiro Silva <paulotec96@gmail.com>
2021-03-23 20:49:16 -0300

    Pattern flyweight was created

---
+++ src/structural/flyweight/delivery/delivery-location.ts
@@ -0,0 +1,19 @@
+import { DeliveryFlyweight } from './delivery-flyweight';
+import { DeliveryLocationData } from './delivery-types';
+
+export class DeliveryLocation implements DeliveryFlyweight {
+  constructor(private readonly instrinsicState: DeliveryLocationData) {}
+  deliver(name: string, number: string): void {
+    console.log('Entrega para %s', name);
+    console.log('Em', this.instrinsicState.street, this.instrinsicState.city);
+    console.log('Número:', number);
+    console.log('###');
+  }
+
+  delivery(name: string, number: string): void {
+    console.log('Entrega para %s', name);
+    console.log('Em ', this.instrinsicState.street, this.instrinsicState.city);
+    console.log('Número: ', number);
+    console.log('###');
+  }
+}
---
+++ src/structural/flyweight/main.ts
@@ -0,0 +1,11 @@
+import { deliveryContext } from './delivery/delivery-context';
+import { DeliveryFactory } from './delivery/delivery-factory';
+
+const factory = new DeliveryFactory();
+deliveryContext(factory, 'Joao', '15A', 'Av Silva', 'SP');
+deliveryContext(factory, 'Maria', '15A', 'Av Silva', 'SP');
+deliveryContext(factory, 'Carla', '16A', 'Av Fonseca', 'SP');
+
+
+console.log();
+console.log(factory.getLocations());
