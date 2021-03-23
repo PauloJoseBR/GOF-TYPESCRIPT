eb9f087ba39cb180fd257774d325a2e6b10b48bc
Paulo José Ribeiro Silva <paulotec96@gmail.com>
2021-03-23 20:56:54 -0300

    Removing white space

--- src/structural/flyweight/main.ts
+++ src/structural/flyweight/main.ts
@@ -6,6 +6,5 @@ deliveryContext(factory, 'Joao', '15A', 'Av Silva', 'SP');
 deliveryContext(factory, 'Maria', '15A', 'Av Silva', 'SP');
 deliveryContext(factory, 'Carla', '16A', 'Av Fonseca', 'SP');
 
-
 console.log();
 console.log(factory.getLocations());
