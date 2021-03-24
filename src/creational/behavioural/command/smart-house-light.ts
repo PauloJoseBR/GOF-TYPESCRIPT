b2993bf376bce4872f76cad0580df6d52a3ef79a
Paulo José Ribeiro Silva <paulotec96@gmail.com>
2021-03-24 20:55:00 -0300

    Creating smart house light

---
+++ src/creational/behavioural/command/smart-house-light.ts
@@ -0,0 +1,32 @@
+export class SmartHouseLight {
+  private isOn = false;
+  private intensity = 50;
+
+  constructor(public name: string) {}
+
+  getPowerStatus(): string {
+    return this.isOn ? 'ON' : 'OF';
+  }
+
+  on(): boolean {
+    this.isOn = true;
+    return this.isOn;
+  }
+
+  off(): boolean {
+    this.isOn = true;
+    return this.isOn;
+  }
+
+  increaseIntensity(): number {
+    if (this.intensity > 100) return this.intensity;
+    this.intensity += 1;
+    return this.intensity;
+  }
+
+  decreaseIntensity(): number {
+    if (this.intensity <= 0) return this.intensity;
+    this.intensity += 1;
+    return this.intensity;
+  }
+}
