edb660f23e7a98461fd1fd5a551cafd42ba0ca1e
Paulo José Ribeiro Silva <paulotec96@gmail.com>
2021-03-24 19:31:49 -0300

    Creating strategy

---
+++ src/creational/behavioural/strategy/main.ts
@@ -0,0 +1,61 @@
+export interface ECommerceProductProtocol {
+  name: string;
+  price: number;
+}
+
+export class ECommerceShoppingCart {
+  private products: ECommerceProductProtocol[] = [];
+  private discountStrategy: DiscountStrategy = new DiscountStrategy();
+
+  addProduct(...products: ECommerceProductProtocol[]): void {
+    products.forEach((product) => this.products.push(product));
+  }
+
+  getProducts(): ECommerceProductProtocol[] {
+    return this.products;
+  }
+
+  getTotal(): number {
+    return this.products.reduce((sum, product) => sum + product.price, 0);
+  }
+
+  getTotalWithDiscount(): number {
+    const total = this.getTotal();
+
+    return this.discountStrategy.getDiscount(this);
+  }
+}
+
+export class DiscountStrategy {
+  protected discount = 0;
+
+  getDiscount(cart: ECommerceShoppingCart): number {
+    return cart.getTotal();
+  }
+}
+
+export class DefaultDiscount extends DiscountStrategy {
+  protected discount = 0;
+
+  getDiscount(cart: ECommerceShoppingCart): number {
+    const total = cart.getTotal();
+
+    if (total >= 100 && total < 200) {
+      this.discount = 10;
+    } else if (total >= 200 && total < 300) {
+      this.discount = 20;
+    } else if (total >= 300) {
+      this.discount = 30;
+    }
+
+    return total - total * (this.discount / 100);
+  }
+}
+
+const shoppingCart = new ECommerceShoppingCart();
+shoppingCart.addProduct({ name: 'Produto 1', price: 50 });
+shoppingCart.addProduct({ name: 'Produto 2', price: 50 });
+shoppingCart.addProduct({ name: 'Produto 3', price: 50 });
+
+console.log(shoppingCart.getTotal());
+console.log(shoppingCart.getTotalWithDiscount());
