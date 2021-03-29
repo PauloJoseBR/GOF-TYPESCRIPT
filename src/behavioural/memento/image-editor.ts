a327e98ff8d33e02c7c0cefc7dd712e99cf42f54
Paulo José Ribeiro Silva <paulotec96@gmail.com>
2021-03-29 18:52:03 -0300

    Creating memento

---
+++ src/behavioural/memento/image-editor.ts
@@ -0,0 +1,28 @@
+//Originator
+
+import { ConcreteMemento } from './concrete-memento';
+import { Memento } from './memento';
+
+export class ImageEditor {
+  constructor(private filePath: string, private fileFormat: string) {}
+
+  convertFormatTo(format: string): void {
+    this.fileFormat = format;
+    this.filePath = this.filePath.split('.').slice(0, -1).join('');
+    this.filePath += '.' + format;
+  }
+
+  save(): Memento {
+    const date = new Date();
+    return new ConcreteMemento(
+      date.toISOString(),
+      date,
+      this.filePath,
+      this.fileFormat,
+    );
+  }
+}
+
+const img = new ImageEditor('/media/imagem.png', 'png');
+img.convertFormatTo('jpg');
+console.log(img);
