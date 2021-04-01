ed35651fdc162a12d80f3c0ba3f6751777af9467
Paulo José Ribeiro Silva <paulotec96@gmail.com>
2021-04-01 20:42:17 -0300

    Adding CEO manager

---
+++ src/behavioural/chain-of-responsibility/ceo-budget-handler.ts
@@ -0,0 +1,9 @@
+import { BaseBudgetHandler } from './base-budget-handler';
+import { CustomerBudget } from './customer-budget';
+
+export class ManagerBudgetHandler extends BaseBudgetHandler {
+  handle(budget: CustomerBudget): CustomerBudget {
+    console.log('O manager tratou o orçamento');
+    return budget;
+  }
+}
