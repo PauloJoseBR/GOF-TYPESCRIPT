6d258df97fad8eb0e1bcc87438f2a1a5ee12a6f7
Paulo José Ribeiro Silva <paulotec96@gmail.com>
2021-04-01 21:02:37 -0300

    Adding client code

--- src/behavioural/chain-of-responsibility/ceo-budget-handler.ts
+++ src/behavioural/chain-of-responsibility/ceo-budget-handler.ts
@@ -1,7 +1,6 @@
 import { BaseBudgetHandler } from './base-budget-handler';
 import { CustomerBudget } from './customer-budget';
-
-export class ManagerBudgetHandler extends BaseBudgetHandler {
+export class CEOBudgetHandler extends BaseBudgetHandler {
   handle(budget: CustomerBudget): CustomerBudget {
     console.log('O CEO tratou o orçamento');
     budget.approved = true;
