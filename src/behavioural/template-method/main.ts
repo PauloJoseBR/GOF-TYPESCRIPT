8ce05d912630251e15a87301611ae86ad7dea8d0
Paulo Jose Ribeiro Silva <paulotec96@gmail.com>
2021-04-02 23:35:40 -0300

    Refactoring names

--- src/behavioural/template-method/main.ts
+++ src/behavioural/template-method/main.ts
@@ -2,11 +2,11 @@ import { resolve } from 'path';
 import { CustomerDataParserTxt } from './customer-data-aparse-txt';
 
 async function run() {
-  const filePath = resolve(__dirname, 'files', 'customer.txt');
-  console.log(filePath);
-  const customerDataParser = new CustomerDataParserTxt(filePath);
-  await customerDataParser.fixCustomerData();
-  console.log(customerDataParser.customerData);
+  const filePathTXT = resolve(__dirname, 'files', 'customer.txt');
+  console.log(filePathTXT);
+  const customerDataParserTXT = new CustomerDataParserTxt(filePathTXT);
+  await customerDataParserTXT.fixCustomerData();
+  console.log(customerDataParserTXT.customerData);
 }
 
 run();
