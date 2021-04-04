74f0140fa215393aab21fedb40ac94c37d888da1
Paulo Jose Ribeiro Silva <paulotec96@gmail.com>
2021-04-04 18:15:08 -0300

    Adding new methods to observer

---
+++ src/behavioural/observer/observer.js
@@ -0,0 +1,40 @@
+var InputObservable = /** @class */ (function () {
+    function InputObservable(input) {
+        this.input = input;
+        this.observers = [];
+    }
+    InputObservable.prototype.subscribe = function () {
+        var _this = this;
+        var observers = [];
+        for (var _i = 0; _i < arguments.length; _i++) {
+            observers[_i] = arguments[_i];
+        }
+        observers.forEach(function (observer) {
+            if (!_this.observers.includes(observer)) {
+                _this.observers.push(observer);
+            }
+        });
+    };
+    InputObservable.prototype.unsubscribe = function (observe) {
+        var observerIndex = this.observers.indexOf(observe);
+        if (observerIndex !== -1) {
+            this.observers.splice(observerIndex, 1);
+        }
+    };
+    InputObservable.prototype.notify = function () {
+        var _this = this;
+        this.observers.forEach(function (observer) { return observer.update(_this); });
+    };
+    return InputObservable;
+}());
+var ParagraphObserver = /** @class */ (function () {
+    function ParagraphObserver(element) {
+        this.element = element;
+    }
+    ParagraphObserver.prototype.update = function (observable) {
+        if (observable instanceof InputObservable) {
+            this.element.innerText = observable.input.value;
+        }
+    };
+    return ParagraphObserver;
+}());
--- src/behavioural/observer/observer.ts
+++ src/behavioural/observer/observer.ts
@@ -7,3 +7,38 @@ interface Observable {
 interface Observer {
   update(...args: unknown[]): void;
 }
+
+class InputObservable implements Observable {
+  private observers: Observer[] = [];
+
+  constructor(public input: HTMLInputElement) {}
+
+  subscribe(...observers: Observer[]): void {
+    observers.forEach((observer) => {
+      if (!this.observers.includes(observer)) {
+        this.observers.push(observer);
+      }
+    });
+  }
+
+  unsubscribe(observe: Observer): void {
+    const observerIndex = this.observers.indexOf(observe);
+
+    if (observerIndex !== -1) {
+      this.observers.splice(observerIndex, 1);
+    }
+  }
+
+  notify(): void {
+    this.observers.forEach((observer) => observer.update(this));
+  }
+}
+
+class ParagraphObserver implements Observer {
+  constructor(public element: HTMLParagraphElement) {}
+  update(observable: Observable): void {
+    if (observable instanceof InputObservable) {
+      this.element.innerText = observable.input.value;
+    }
+  }
+}
