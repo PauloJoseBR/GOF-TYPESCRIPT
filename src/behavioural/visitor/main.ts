bd79ef84d840b23af8d8765c3bc269a1d1a5f6b1
Paulo Jose Ribeiro Silva <paulotec96@gmail.com>
2021-04-05 01:38:36 -0300

    Adding german taxes on client

--- src/behavioural/visitor/main.ts
+++ src/behavioural/visitor/main.ts
@@ -2,6 +2,7 @@ import { AlcholicDrink } from './alcoholic-drink';
 import { BrazilTaxVisitor } from './brazil-tax-visitor';
 import { Cigarette } from './cigarette';
 import { Food } from './food';
+import { GermanyTaxVisitor } from './germany-tax-visitor';
 
 const food = new Food(10);
 const cigarette = new Cigarette(5);
@@ -10,12 +11,19 @@ const drink = new AlcholicDrink(5);
 const cart = [food, cigarette, drink];
 
 const brazilTaxVisitor = new BrazilTaxVisitor();
+const germanTaxVisitor = new GermanyTaxVisitor();
 
 const total = cart.reduce((sum, item) => item.getPrice() + sum, 0);
-const totalWithTaxes = cart.reduce(
+const totalWithBrazilianTaxes = cart.reduce(
   (sum, item) => item.getPriceWithTaxes(brazilTaxVisitor) + sum,
   0,
 );
 
+const totalWithGermanTaxes = cart.reduce(
+  (sum, item) => item.getPriceWithTaxes(germanTaxVisitor) + sum,
+  0,
+);
+
 console.log(total);
-console.log(totalWithTaxes);
+console.log(totalWithBrazilianTaxes);
+console.log(totalWithGermanTaxes);
