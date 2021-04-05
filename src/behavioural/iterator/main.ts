a9a44a9ecbef15638f5a3a2aa8755b1245e66bd4
Paulo Jose Ribeiro Silva <paulotec96@gmail.com>
2021-04-04 22:15:19 -0300

    Changing client code

--- src/behavioural/iterator/main.ts
+++ src/behavioural/iterator/main.ts
@@ -2,4 +2,11 @@ import { MyDataStructure } from './my-data-structure';
 
 const dataStructure = new MyDataStructure();
 dataStructure.addItem('A', 'B', 'C');
-console.log(dataStructure);
+
+const [a, b] = dataStructure;
+
+console.log('ROUBADOS: ', a, b);
+
+for (const data of dataStructure) {
+  console.log(data);
+}
