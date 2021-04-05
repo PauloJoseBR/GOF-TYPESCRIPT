d04505430a398aa77cafac36f856a9c3241dad23
Paulo Jose Ribeiro Silva <paulotec96@gmail.com>
2021-04-04 23:49:10 -0300

    Finishing iterator pattern

--- src/behavioural/iterator/main.ts
+++ src/behavioural/iterator/main.ts
@@ -2,23 +2,29 @@ import { MyDataStructure } from './my-data-structure';
 import { MyReverseIterator } from './my-reverse-iterator';
 
 const dataStructure = new MyDataStructure();
-dataStructure.addItem('A', 'B', 'C');
+dataStructure.addItem('A', 'B', 'C', 'D', 'E', 'F');
 
 const [a, b] = dataStructure;
-
-console.log('STOLEN: ', a, b);
-console.log('EXECUTED MANY THINGS THEN I USED ITERATOR');
-const [c, ...rest] = dataStructure;
-console.log(c, rest);
+console.log('STOLEN:', a, b);
+console.log('HAD EXECUTED MANY THINGS THAN I USED ITERATOR');
+const [c, d, ...rest] = dataStructure;
+console.log(c, d, rest);
 
 dataStructure.resetIterator();
 for (const data of dataStructure) {
   console.log(data);
 }
-
 console.log();
 dataStructure.changeIterator(new MyReverseIterator(dataStructure));
 
 for (const data of dataStructure) {
   console.log(data);
 }
+console.log();
+
+console.log('Must RESET Iterator');
+dataStructure.resetIterator();
+for (const data of dataStructure) {
+  console.log(data);
+}
+console.log();
