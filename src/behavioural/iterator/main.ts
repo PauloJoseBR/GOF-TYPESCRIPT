865cf3f9974fbc008b22813f640e12c21281d915
Paulo Jose Ribeiro Silva <paulotec96@gmail.com>
2021-04-04 22:36:59 -0300

    Changing client code

--- src/behavioural/iterator/main.ts
+++ src/behavioural/iterator/main.ts
@@ -5,7 +5,13 @@ dataStructure.addItem('A', 'B', 'C');
 
 const [a, b] = dataStructure;
 
-console.log('ROUBADOS: ', a, b);
+console.log('STOLEN: ', a, b);
+
+for (const data of dataStructure) {
+  console.log(data);
+}
+
+console.log("I don't have values anymore");
 
 for (const data of dataStructure) {
   console.log(data);
